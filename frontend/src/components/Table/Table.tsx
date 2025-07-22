1896e835fdc909f4b37e16a91203b19c7101baef
murashko12 <petr@ocumare.ru>
2025-07-22 17:31:36 +0300

    fix: display message of empty users list

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -35,12 +35,15 @@ const Table: FC<IProps> & SubComponents = ({
     isLoading
 }) => {
     return (
-        <div className="max-h-[500px] overflow-auto rounded-t-xl mt-5 bg-[#131524] border border-[#4A516D]">
+        <div className="max-h-[500px] overflow-auto rounded-xl mt-5 bg-[#131524] border border-[#4A516D]">
             <table className="table table-xs table-pin-rows table-pin-cols text-center ">
                 <TableContext.Provider value={{ items, headers, isLoading }}>
                     {children}
                 </TableContext.Provider>
             </table>
+            {!items || !items.length && <div className="border-[#4A516D] h-20 flex items-center justify-center">
+                <p className="font-bold">Список пользователей пуст</p>
+            </div>}
         </div>
     )
 }
